7561a991d777f24a6b38c16bc59b62a921065b4e
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2023-12-28 23:34:33 -0500

    Commented out ES5 code to avoid error on console

--- exercises.js
+++ exercises.js
@@ -1,11 +1,11 @@
 //old ES5 code
 
-function filterOutOdds() {
-  var nums = Array.prototype.slice.call(arguments);
-  return nums.filter(function (num) {
-    return num % 2 === 0;
-  });
-}
+// function filterOutOdds() {
+//   var nums = Array.prototype.slice.call(arguments);
+//   return nums.filter(function (num) {
+//     return num % 2 === 0;
+//   });
+// }
 
 //my attempt
 

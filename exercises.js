f02bfb52e646cc94c03b5588bea7ab9b8f20a5c5
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2023-12-28 23:34:10 -0500

    added index.html add findMin code

--- exercises.js
+++ exercises.js
@@ -18,7 +18,7 @@ const filterOutOdds = (...args) => args.filter((v) => v % 2 === 0);
 //Write a function called findMin that accepts a variable number of arguments and returns the smallest argument.
 //Make sure to do this using the rest and spread operator.
 
-const findMin = undefined;
+const findMin = (...nums) => Math.min(nums);
 
 //Write a function called mergeObjects that accepts two objects
 // and returns a new object which contains all the keys and values of the first object and second object.

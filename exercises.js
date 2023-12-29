6d8b7f8fb9c60c2f717a8e01559c93f2fdb0f5ad
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2023-12-29 00:29:13 -0500

    combine and update attempts

--- exercises.js
+++ exercises.js
@@ -59,8 +59,12 @@ const removeKey = (obj, key) => {
 
 /** Combine two objects and return a new object. */
 
-function combine(obj1, obj2) {}
+const combine = (obj1, obj2) => {
+  return { ...obj1, ...obj2 };
+};
 
 /** Return a new object with a modified key and value. */
 
-function update(obj, key, val) {}
+const update = (obj, key, val) => {
+  return { [key]: val, ...obj };
+};

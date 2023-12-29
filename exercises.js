5c494088be41088e07e79ea73b2298cb336d7bd3
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2023-12-28 23:23:53 -0500

    Copying over first exercises, first attempt

---
+++ exercises.js
@@ -0,0 +1,18 @@
+//old ES5 code
+
+function filterOutOdds() {
+  var nums = Array.prototype.slice.call(arguments);
+  return nums.filter(function(num) {
+    return num % 2 === 0
+  });
+}
+
+//my attempt
+
+const filterOutOdds = (...nums) => (...nums).filter(num => num % 2 === 0)
+
+//solution - needed to remove parenth after first arrow
+
+const filterOutOdds = (...args) => args.filter(v => v % 2 === 0)
+
+// 

7c058faacd325b3993066cdae5aef0d7665196bb
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2023-12-28 23:24:20 -0500

    Commented out my attempt to avoid errors

--- exercises.js
+++ exercises.js
@@ -2,17 +2,17 @@
 
 function filterOutOdds() {
   var nums = Array.prototype.slice.call(arguments);
-  return nums.filter(function(num) {
-    return num % 2 === 0
+  return nums.filter(function (num) {
+    return num % 2 === 0;
   });
 }
 
 //my attempt
 
-const filterOutOdds = (...nums) => (...nums).filter(num => num % 2 === 0)
+//const filterOutOdds = (...nums) => (...nums).filter(num => num % 2 === 0)
 
 //solution - needed to remove parenth after first arrow
 
-const filterOutOdds = (...args) => args.filter(v => v % 2 === 0)
+const filterOutOdds = (...args) => args.filter((v) => v % 2 === 0);
 
-// 
+//

53be405a9c53d15af218f0b035cd3a10ed561fea
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2023-12-28 23:46:52 -0500

    mergeObjects attempt

--- exercises.js
+++ exercises.js
@@ -23,7 +23,7 @@ const findMin = (...nums) => Math.min(...nums);
 //Write a function called mergeObjects that accepts two objects
 // and returns a new object which contains all the keys and values of the first object and second object.
 
-const mergeObjects = undefined;
+const mergeObjects = (obj1, obj2) => ({ ...obj1, ...obj2 });
 
 //Write a function called doubleAndReturnArgs which accepts an array and a variable number of arguments.
 //The function should return a new array with the original array values and all of additional arguments doubled.
